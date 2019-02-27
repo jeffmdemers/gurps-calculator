4b572dff58819786c22b9940a65bccc20130e5b5
Jeff Demers <jeff@motivislearning.com>
2019-02-27 16:49:21 -0500

    menu system, sidenav, gitpages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gurps-calculator';
-
+  sidebarOpened: boolean;
   constructor() {
   }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+class MenuItem {
+  text: string;
+  link: string;
+  icon: string;
+
+  constructor(text: string, link: string, icon: string) {
+    this.text = text;
+    this.link = link;
+    this.icon = icon;
+  }
+}
+
+class MenuGroup {
+  header: string;
+  menuItems: MenuItem[];
+
+  constructor(header: string, menuItems: MenuItem[]) {
+    this.header = header;
+    this.menuItems = menuItems;
+  }
+}
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +28,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+  menuGroups: MenuGroup[];
+  @Output() itemClicked: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+    this.loadMenuItems();
   }
+
+  itemSelected() {
+    this.itemClicked.emit();
+  }
+
+  loadMenuItems() {
+    const utility_menuItems = [
+      new MenuItem('Dice Roller', '/dice-roller', 'casino'),
+      new MenuItem('Jumping', '/jumping', 'directions_run'),
+      new MenuItem('Throwing', '/throwing', 'gps_not_fixed')
+    ];
+
+    this.menuGroups = [
+      new MenuGroup('', [new MenuItem('Home', '/', 'home')]),
+      new MenuGroup('Utilities', utility_menuItems)
+    ];
+  }
+
 
 }
