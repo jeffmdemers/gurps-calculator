2d369351917211430d0059d28a35e9398db67ba3
Jeff Demers <jeff@motivislearning.com>
2019-03-01 16:47:08 -0500

    check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gurps-calculator';
-  sidebarOpened: boolean;
+  appSelected = 'dice-roller';
   constructor() {
   }
 }
